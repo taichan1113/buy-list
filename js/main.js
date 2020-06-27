8bee7fc090bfd04a19b2373b9c0386147cc85a67
taichan <takecchi.1113@gmail.com>
2020-06-27 12:05:48 +0900

    check function is OK

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
 'use strict';
 {
 
-let submit_cart = document.getElementById('submit');
-let buy_list = document.getElementById('buy-list');
+let submit_cart = document.getElementById('submit'  );
+let buy_list    = document.getElementById('buy-list');
 
 submit_cart.addEventListener('click', () => {
-  let li = document.createElement('li');
-  li.innerText = document.getElementById('input').value;
-  buy_list.appendChild(li);
-  document.getElementById('input').value = null;
+  if  (document.getElementById('input').value !== '') {
+    let list  = document.createElement('div'  );
+    let check = document.createElement('input');
+    let p     = document.createElement('p'    );
+    check.type = 'checkbox';
+    list.appendChild(check);
+    p.innerText = document.getElementById('input').value;
+    list.appendChild(p);
+    buy_list.appendChild(list);
+  }
+  document.getElementById('input').value = '';
   document.getElementById('input').focus();
-});
+
+    console.log('list is added');
+  });
 
 }
