d4773af6bc471f5839fb3979fe0aa15344a386d7
taichan <takecchi.1113@gmail.com>
2020-06-27 16:50:58 +0900

    class Cart and CheckList are created

--- js/main.js
+++ js/main.js
@@ -1,34 +1,50 @@
 'use strict';
 {
 
-let submit_cart = document.getElementById('submit'  );
-let buy_list    = document.getElementById('buy-list');
-
-submit_cart.addEventListener('click', () => {
-  if  (document.getElementById('input').value !== '') {
-    let list  = document.createElement('div'  );
-
-    let check = document.createElement('input');
-    check.type = 'checkbox';
-
-    let label = document.createElement('label');
-    label.innerText = document.getElementById('input').value;
-    label.appendChild(check);
-
-    label.firstElementChild.addEventListener('change', () => {
-      if (label.className === 'checked') {
-        label.classList.remove('checked');
-      } else {
-        label.classList.add('checked');
-      }
-    });
-
-    list.appendChild(label);
-    buy_list.appendChild(list);
+class Cart {
+  constructor(){
+    this.btn  = document.getElementById('submit'   );
+    this.list = document.getElementById('buy-list' );
+    this.item = document.getElementById('input'    );
   }
-  document.getElementById('input').value = '';
-  document.getElementById('input').focus();
 
-  });
+  event() {
+    this.btn.addEventListener('click', () => {
+      let check_list = new CheckList(this.item.value);
+      check_list.listening();
+      this.item.value = '';
+      this.item.focus();
+      this.list.appendChild(check_list.list);
+    });
+  }
+} //end of class Cart
+
+class CheckList {
+  constructor(list_text) {
+    this.text = list_text;
+    this.list = document.createElement('div'  );
+    this.check = document.createElement('input');
+    this.check.type = 'checkbox';
+    this.label = document.createElement('label');
+    this.label.innerText = this.text;
+    this.label.appendChild(this.check);
+    this.list.appendChild(this.label);
+  }
+
+  listening() {
+    if (this.text !== '') {
+      this.check.addEventListener('change', () => {
+        if (this.label.className === 'checked') {
+          this.label.classList.remove('checked');
+        } else {
+          this.label.classList.add('checked');
+        }
+      });
+    }
+  }
+} // end of class CheckList
+
+let cart = new Cart();
+cart.event();
 
 }
