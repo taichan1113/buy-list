8ef8291de0f5e277d0196d9f9b1618db18acd6e5
taichan <takecchi.1113@gmail.com>
2020-06-27 14:26:37 +0900

    line-thorough

--- js/main.js
+++ js/main.js
@@ -8,17 +8,24 @@ submit_cart.addEventListener('click', () => {
   if  (document.getElementById('input').value !== '') {
     let list  = document.createElement('div'  );
     let check = document.createElement('input');
-    let p     = document.createElement('p'    );
     check.type = 'checkbox';
+    let label = document.createElement('label');
+    label.innerText = document.getElementById('input').value;
     list.appendChild(check);
-    p.innerText = document.getElementById('input').value;
-    list.appendChild(p);
+    list.appendChild(label);
+    list.firstElementChild.addEventListener('change', () => {
+      // let check_judge = list.lastElementChild.classList;
+      if (list.lastElementChild.className === 'checked') {
+        list.lastElementChild.classList.remove('checked');
+      } else {
+        list.lastElementChild.classList.add('checked');
+      }
+    });
     buy_list.appendChild(list);
   }
   document.getElementById('input').value = '';
   document.getElementById('input').focus();
 
-    console.log('list is added');
   });
 
 }
